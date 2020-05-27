182e92d70a604c6be41626076b029419eae9d0cc
Kurt Warwick <kurtw@mda.local>
2020-05-27 16:36:37 +0200

    debugging

--- src/platform_temp.js
+++ src/platform_temp.js
@@ -1,46 +1,81 @@
 const appletv = require("node-appletv-x");
 const Device = require("./device");
 
-function Platform(log, config, api) {
-    this.log = log;
-    this.config = config;
-    this.api = api;
+class Platform {
+    constructor(log, config, api) {
+        this.log = log;
+        this.config = config;
+        this.api = api;
+        this.accessories = [];
+        this.devices = [];
+        
+        this.api.on("didFinishLaunching", this.onApiDidFinishLaunching);
+    }
 
-    this.debug = message => {
+    debug = message => {
         if (this.config && this.config.debug) {
             this.log(message);
         }
-    }
+    };
 
-    this.api.on("didFinishLaunching", async () => {
-        config.devices.map(async (devicesConfiguration) => {
-            try {
-                let credentials = appletv.parseCredentials(devicesConfiguration.credentials);
+    configureAccessory = accessory => {
+        if (!accessory.context.uid) {
+            this.debug(`Removing cached accessory width id ${accessory.UUID}`);
 
-                config.verbose && console.log(`scanning for apple tv with identifier ${credentials.uniqueIdentifier}.`);
+            this.api.unregisterPlatformAccessories(Platform.pluginName, Platform.platformName, [accessory]);
+        }
+        else {
+            this.accessories.push(accessory);
 
-                let devices = await appletv.scan(credentials.uniqueIdentifier);
+            this.debug(`Loaded cached accessory width id ${accessory.UUID}`);
+        }
+    };
 
-                config.verbose && console.log(`apple tv with identifier ${credentials.uniqueIdentifier} found.`);
-                config.verbose && console.log(`attempting to connect to apple tv with identifier ${credentials.uniqueIdentifier}.`);
-
-                let connectedDevice = await devices[0].openConnection(credentials);
-
-                config.verbose && console.log(`connected to apple tv with identifier ${credentials.uniqueIdentifier}.`);
-
-                connectedDevice.on("nowPlaying", this.debug);
-                //connectedDevice.on("supportedCommands", this.debug);
-
-                setInterval(
-                    () =>
-                        //connectedDevice.sendIntroduction().then(this.debug),
-                    5000
-                );
-            } catch (error) {
-                console.error(error);
-            }
+    cleanupAccessory = accessory => {
+        let foundAccessory = this.config.devices.filter(deviceConfiguration => {
+            let credentials = appletv.parseCredentials(deviceConfiguration.credentials);
+            accessory.UUID === `${credentials.uniqueIdentifier}_apple_tv`;
         });
-    });
+
+        if (!foundAccessory) {
+            this.debug(`Removing orphaned accessory [${accessory.uid}].`);
+
+            this.unregisterAccessories([accessory]);
+        }
+    };
+
+    loadDevice = deviceConfiguration => {
+        let credentials = appletv.parseCredentials(deviceConfiguration.credentials);
+
+        config.verbose && console.log(`Scanning for Apple TV [${credentials.uniqueIdentifier}].`);
+
+        let devices = await appletv.scan(credentials.uniqueIdentifier);
+
+        config.verbose && console.log(`Apple TV [${credentials.uniqueIdentifier}] found.`);
+        config.verbose && console.log(`Attempting to connect to Apple TV [${credentials.uniqueIdentifier}].`);
+
+        let connectedDevice = await devices[0].openConnection(credentials);
+
+        config.verbose && console.log(`Connected to ${connectedDevice.name} [${connectedDevice.uid}].`);
+        config.verbose && console.log(`Loading acessory for ${connectedDevice.name} [${connectedDevice.uid}].`);
+
+        this.devices.push(new Device(this, deviceConfiguration, connectedDevice));
+    };
+
+    onApiDidFinishLaunching = () => {
+        if (!this.config.devices) {
+            this.debug("No Apple TV devices have been configured.");
+            return;
+        }
+
+        this.debug("Cleaning up orphaned accessories...");
+
+        this.accessories.map(this.cleanupAccessory);
+
+        this.debug("Loading configured Apple TVs...");
+        
+        this.config.devices.map(this.loadDevice);
+    };
 }
 
 Platform.pluginName = "homebridge-appletv-now-playing";
