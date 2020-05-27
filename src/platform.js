bcf4e067207b59fbcb326f71b42a8526952aebbb
Kurt Warwick <kurtw@mda.local>
2020-05-27 12:28:19 +0200

    CLeaned up scanning and connection process

--- src/platform.js
+++ src/platform.js
@@ -2,97 +2,101 @@ const appletv = require("node-appletv-x");
 const Device = require("./device");
 
 function Platform(log, config, api) {
-  this.log = log;
-  this.config = config;
-  this.api = api;
+    this.log = log;
+    this.config = config;
+    this.api = api;
 
-  this.accessories = [];
-  this.devices = [];
+    this.accessories = [];
+    this.devices = [];
 
-  this.debug = debug.bind(this);
-  this.registerAccessories = registerAccessories.bind(this);
-  this.unregisterAccessories = unregisterAccessories.bind(this);
-  this.updateAccessories = updateAccessories.bind(this);
-  this.configureAccessory = configureAccessory.bind(this);
-  this.apiDidFinishLaunching = apiDidFinishLaunching.bind(this);
-  this.onScanComplete = onScanComplete.bind(this);
-  this.scanForDevice = scanForDevice.bind(this);
-  this.onDeviceConnected = onDeviceConnected.bind(this);
-  this.onDeviceConnectionFailed = onDeviceConnectionFailed.bind(this);
+    this.debug = debug.bind(this);
+    this.registerAccessories = registerAccessories.bind(this);
+    this.unregisterAccessories = unregisterAccessories.bind(this);
+    this.updateAccessories = updateAccessories.bind(this);
+    this.configureAccessory = configureAccessory.bind(this);
+    this.apiDidFinishLaunching = apiDidFinishLaunching.bind(this);
+    this.onScanComplete = onScanComplete.bind(this);
+    this.scanForDevice = scanForDevice.bind(this);
+    this.onDeviceConnected = onDeviceConnected.bind(this);
+    this.onDeviceConnectionFailed = onDeviceConnectionFailed.bind(this);
 
-  function registerAccessories(accessories) {
-    this.api.registerPlatformAccessories(Platform.pluginName, Platform.platformName, accessories);
-  }
-
-  function unregisterAccessories(accessories) {
-    this.api.unregisterPlatformAccessories(Platform.pluginName, Platform.platformName, accessories);
-  }
-
-  function updateAccessories(accessories) {
-    this.api.updatePlatformAccessories(accessories);
-  }
-
-  function debug(message) {
-    if (this.config && this.config.debug) {
-      this.log(message);
-    }
-  }
-
-  function configureAccessory(accessory) {
-    if (!accessory.context.uid) {
-      this.debug(`Removing cached accessory width id ${accessory.UUID}`);
-
-      this.api.unregisterPlatformAccessories(Platform.pluginName, Platform.platformName, [accessory]);
-    } else {
-      this.accessories.push(accessory);
-
-      this.debug(`Loaded cached accessory width id ${accessory.UUID}`);
-    }
-  }
-
-  function apiDidFinishLaunching() {
-    if (!this.config.devices) {
-      this.debug("No devices have bene configured.");
-
-      return;
+    function registerAccessories(accessories) {
+        this.api.registerPlatformAccessories(Platform.pluginName, Platform.platformName, accessories);
     }
 
-    this.debug("Scanning for Apple TVs...");
+    function unregisterAccessories(accessories) {
+        this.api.unregisterPlatformAccessories(Platform.pluginName, Platform.platformName, accessories);
+    }
 
-    config.devices.map(this.scanForDevice);
-  }
+    function updateAccessories(accessories) {
+        this.api.updatePlatformAccessories(accessories);
+    }
 
-  function scanForDevice(deviceConfiguration) {
-    let credentials = appletv.parseCredentials(deviceConfiguration.credentials);
+    function debug(message) {
+        if (this.config && this.config.debug) {
+            this.log(message);
+        }
+    }
 
-    this.debug(`Scanning for Apple TV with identifier ${credentials.uniqueIdentifier}.`);
+    function configureAccessory(accessory) {
+        if (!accessory.context.uid) {
+            this.debug(`Removing cached accessory width id ${accessory.UUID}`);
 
-    appletv.scan(credentials.uniqueIdentifier).then(this.onScanComplete);
-  }
+            this.api.unregisterPlatformAccessories(Platform.pluginName, Platform.platformName, [accessory]);
+        } else {
+            this.accessories.push(accessory);
 
-  function onScanComplete(devices) {
-    this.debug(`Found Apple TV with identifier ${credentials.uniqueIdentifier}.`);
-    this.debug(`Attempting to connect to Apple TV with identifier ${credentials.uniqueIdentifier}.`);
+            this.debug(`Loaded cached accessory width id ${accessory.UUID}`);
+        }
+    }
 
-    let device = devices[0];
+    function apiDidFinishLaunching() {
+        if (!this.config.devices) {
+            this.debug("No devices have bene configured.");
 
-    device.openConnection(device.credentials).then(device => this.onDeviceConnected(deviceConfiguration, device), this.onDeviceConnectionFailed);
+            return;
+        }
 
-    this.debug("Scanning complete.");
-  }
+        this.debug("Scanning for Apple TVs...");
 
-  function onDeviceConnected(deviceConfiguration, device) {
-    this.devices.push(new Device(this, deviceConfiguration, device));
+        config.devices.map(this.scanForDevice);
+    }
 
-    this.debug(`Connected to Apple TV with id ${device.uid}.`);
-  }
+    function scanForDevice(deviceConfiguration) {
+        let credentials = appletv.parseCredentials(deviceConfiguration.credentials);
 
-  function onDeviceConnectionFailed(device) {
-    this.debug(device);
-    this.debug(`Unable to connect to Apple TV.`);
-  }
+        this.debug(`Scanning for Apple TV with identifier ${credentials.uniqueIdentifier}.`);
 
-  this.api.on("didFinishLaunching", this.apiDidFinishLaunching);
+        appletv.scan(credentials.uniqueIdentifier).then((devices) => this.onScanComplete(devices, credentials));
+    }
+
+    function onScanComplete(devices, credentials) {
+        if (devices.length) {
+            let device = devices[0];
+
+            this.debug(`Found Apple TV ${device.name} [${device.uid}].`);
+            this.debug(`Attempting to connect to Apple TV ${device.name} [${device.uid}].`);
+
+            device[0].openConnection(device.credentials).then((device) => this.onDeviceConnected(deviceConfiguration, device), this.onDeviceConnectionFailed);
+        } else {
+            this.debug(`Unable to find Apple TV with identifier ${credentials.uniqueIdentifier}. Please try and pair with your device again.`);
+        }
+
+        this.debug("Scanning complete.");
+    }
+
+    function onDeviceConnected(deviceConfiguration, device) {
+        this.devices.push(new Device(this, deviceConfiguration, device));
+
+        this.debug(`Connected to ${device.name} [${device.uid}].`);
+    }
+
+    function onDeviceConnectionFailed(device) {
+        this.debug(device);
+        this.debug(`Unable to connect to Apple TV.`);
+    }
+
+    this.api.on("didFinishLaunching", this.apiDidFinishLaunching);
 }
 
 Platform.pluginName = "homebridge-appletv-now-playing";
